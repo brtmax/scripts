0c13305327bf870160e3db55f47e78a7db173bb6
Max Bretschneider <maxbretschneider@mailbox.org>
2023-05-27 14:49:13 +0200

    Added handling for case where there is no installation candidate, added duplicate check

--- save_install.sh
+++ save_install.sh
@@ -3,22 +3,30 @@
 # Path to file to save the package names
 SAVE_DIR="/home/max/dotfiles/packages.txt"
 
-# Prompt user to save package name
-read -p "Do you want to save the package name? (y/n): " response
-if [[ $response =~ ^[Yy]$ ]]; then
-  read -p "Enter the file name: " filename
-  echo $1 >> "$SAVE_DIR/$filename"
-  echo >> "$SAVE_DIR/$filename"  # Add an empty line
+# Change to dotfiles directory
+cd "$SAVE_DIR"
 
-  # Change to dotfiles directory
-  cd /home/max/dotfiles
+# Check if package name already exists in the file
+if grep -Fxq "$1" "$SAVE_DIR"; then
+  echo "Package name already exists in $SAVE_DIR. Skipping..."
+else
+  # Check if package name is a valid installation candidate
+  if sudo apt-cache show "$1" >/dev/null 2>&1; then
+    echo "$1" >> "$SAVE_DIR"
+    echo >> "$SAVE_DIR"  # Add an empty line
 
-  # Add, commit, and push changes to the repository
-  git add .
-  git commit -m "Saved $1"
-  git push
+    # Add, commit, and push changes to the repository
+    git add .
+    git commit -m "Saved package: $1"
+    git push
+
+    # Install the package
+    sudo apt install "$1"
+  else
+    echo "Package name '$1' is not a valid installation candidate."
+  fi
 fi
 
-# Install the package
-sudo apt install $1
+# Navigate back to the directory where the script was called from
+cd -
 
