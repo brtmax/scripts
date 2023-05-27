3a92447fba1ac119377abf3cc0d7deff90a7552f
Max Bretschneider <maxbretschneider@mailbox.org>
2023-05-27 15:07:57 +0200

    Adjusted indentation for better readability

--- save_install.sh
+++ save_install.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
 # Path to file to save the package names
-SAVE_DIR="/home/max/dotfiles/packages.txt"
+SAVE_FILE_PATH="/home/max/dotfiles/packages.txt"
 
-# Change to dotfiles directory
-cd "$SAVE_DIR"
-
-# Check if package name already exists in the file
-if grep -Fxq "$1" "$SAVE_DIR"; then
-  echo "Package name already exists in $SAVE_DIR. Skipping..."
-else
-  # Check if package name is a valid installation candidate
-  if sudo apt-cache show "$1" >/dev/null 2>&1; then
-    echo "$1" >> "$SAVE_DIR"
-    echo >> "$SAVE_DIR"  # Add an empty line
-
-    # Add, commit, and push changes to the repository
-    git add .
-    git commit -m "Saved package: $1"
-    git push
-
-    # Install the package
-    sudo apt install "$1"
-  else
-    echo "Package name '$1' is not a valid installation candidate."
-  fi
+# Check if SAVE_FILE_PATH is a valid file
+if [ ! -f "$SAVE_FILE_PATH" ]; then
+  echo "Error: Invalid save file path '$SAVE_FILE_PATH'. Aborting."
+  exit 1
 fi
+
+# Check if package name is a valid installation candidate
+if ! apt-cache show "$1" >/dev/null 2>&1; then
+  echo "Package name '$1' is not a valid installation candidate."
+  exit 1
+fi
+
+# Change to save file directory
+cd "$(dirname "$SAVE_FILE_PATH")"
+
+# Check if package name exists in the file
+if grep -Fxq "$1" "$SAVE_FILE_PATH"; then
+  echo "Package name already exists in $SAVE_FILE_PATH. Skipping..."
+else
+  echo "$1" >> "$SAVE_FILE_PATH"
+  echo >> "$SAVE_FILE_PATH"  # Add an empty line
+
+  # Add, commit, and push changes to the repository
+  git add .
+  git commit -m "Saved package: $1"
+  git push
+fi
+
+# Install the package
+sudo apt install "$1"
 
 # Navigate back to the directory where the script was called from
 cd -
-
